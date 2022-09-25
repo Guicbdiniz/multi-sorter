d933b043ae6bb51a6f8283ec55d53dd91b39cf6c
Guicbdiniz <guicbdeniz@hotmail.com>
2022-09-25 18:34:27 -0300

    feat: update selection sort

--- src/sorting/algorithms/selection.ts
+++ src/sorting/algorithms/selection.ts
@@ -3,8 +3,28 @@ import { sortingAlgorithm } from "../types";
 
 export default class SelectionSorter extends Sorter {
   public sort(array: number[]): number[] {
-    return array;
+    let sortedArray = [...array];
+    let arrayLength = sortedArray.length;
+
+    for (let i = 0; i < arrayLength; i++) {
+      let min = i;
+
+      for (let j = i + 1; j < arrayLength; j++) {
+        if (sortedArray[j] < sortedArray[min]) {
+          min = j;
+        }
+      }
+
+      if (min != i) {
+        let tmp = sortedArray[i];
+        sortedArray[i] = sortedArray[min];
+        sortedArray[min] = tmp;
+      }
+    }
+
+    return sortedArray;
   }
+
   public getType(): sortingAlgorithm {
     return "selection";
   }
