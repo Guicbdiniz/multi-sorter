a2a2f71cc7490ce29402d223248d6ad889ce1646
Guicbdiniz <guicbdeniz@hotmail.com>
2022-09-25 18:49:10 -0300

    feat: update shell sort

--- src/sorting/algorithms/shell.ts
+++ src/sorting/algorithms/shell.ts
@@ -3,8 +3,33 @@ import { sortingAlgorithm } from "../types";
 
 export default class ShellSorter extends Sorter {
   public sort(array: number[]): number[] {
-    return array;
+    let sortedArray = [...array];
+
+    var increment = sortedArray.length / 2;
+
+    while (increment > 0) {
+      for (let i = increment; i < sortedArray.length; i++) {
+        var j = i;
+        var temp = sortedArray[i];
+
+        while (j >= increment && sortedArray[j - increment] > temp) {
+          sortedArray[j] = sortedArray[j - increment];
+          j = j - increment;
+        }
+
+        sortedArray[j] = temp;
+      }
+
+      if (increment == 2) {
+        increment = 1;
+      } else {
+        increment = Math.trunc((increment * 5) / 11);
+      }
+    }
+
+    return sortedArray;
   }
+
   public getType(): sortingAlgorithm {
     return "shell";
   }
