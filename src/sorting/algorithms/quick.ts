ef22b65008d39df811026c4c057bf4a197a0e869
Guicbdiniz <guicbdeniz@hotmail.com>
2022-09-25 18:44:58 -0300

    feat: update quick sort

--- src/sorting/algorithms/quick.ts
+++ src/sorting/algorithms/quick.ts
@@ -3,8 +3,49 @@ import { sortingAlgorithm } from "../types";
 
 export default class QuickSorter extends Sorter {
   public sort(array: number[]): number[] {
-    return array;
+    let sortedArray = [...array];
+
+    let stack: number[] = [];
+
+    stack.push(0);
+    stack.push(sortedArray.length - 1);
+
+    while (stack[stack.length - 1] >= 0) {
+      let end = stack.pop() as number;
+      let start = stack.pop() as number;
+
+      let pivotIndex = this.partition(sortedArray, start, end);
+
+      if (pivotIndex - 1 > start) {
+        stack.push(start);
+        stack.push(pivotIndex - 1);
+      }
+
+      if (pivotIndex + 1 < end) {
+        stack.push(pivotIndex + 1);
+        stack.push(end);
+      }
+    }
+
+    return sortedArray;
   }
+
+  partition(array: number[], start: number, end: number) {
+    const pivotValue = array[end];
+    let pivotIndex = start;
+
+    for (let i = start; i < end; i++) {
+      if (array[i] < pivotValue) {
+        [array[i], array[pivotIndex]] = [array[pivotIndex], array[i]];
+        pivotIndex++;
+      }
+    }
+
+    [array[pivotIndex], array[end]] = [array[end], array[pivotIndex]];
+
+    return pivotIndex;
+  }
+
   public getType(): sortingAlgorithm {
     return "quick";
   }
