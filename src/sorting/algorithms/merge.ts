7364262e3be3bd0a8aed29c36f287ff597175646
Guicbdiniz <guicbdeniz@hotmail.com>
2022-09-25 18:39:14 -0300

    feat: update merge sort

--- src/sorting/algorithms/merge.ts
+++ src/sorting/algorithms/merge.ts
@@ -3,8 +3,42 @@ import { sortingAlgorithm } from "../types";
 
 export default class MergeSorter extends Sorter {
   public sort(array: number[]): number[] {
-    return array;
+    let sorterArray = [...array];
+
+    return this.mergeSort(sorterArray);
   }
+
+  mergeSort(array: number[]): number[] {
+    const half = array.length / 2;
+
+    if (array.length < 2) {
+      return array;
+    }
+
+    const left = array.splice(0, half);
+    return this.merge(this.mergeSort(left), this.mergeSort(array));
+  }
+
+  merge(left: number[], right: number[]): number[] {
+    let arr: number[] = [];
+
+    while (left.length && right.length) {
+      if (left[0] < right[0]) {
+        const value = left.shift();
+        if (value) {
+          arr.push();
+        }
+      } else {
+        const value = right.shift();
+        if (value) {
+          arr.push(value);
+        }
+      }
+    }
+
+    return [...arr, ...left, ...right];
+  }
+
   public getType(): sortingAlgorithm {
     return "merge";
   }
