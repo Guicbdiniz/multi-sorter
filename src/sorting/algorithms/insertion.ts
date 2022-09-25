9a7c9212e2938751a9eef4623410ca6b4b71c621
Guicbdiniz <guicbdeniz@hotmail.com>
2022-09-25 18:31:58 -0300

    feat: update insetion sort

--- src/sorting/algorithms/insertion.ts
+++ src/sorting/algorithms/insertion.ts
@@ -3,7 +3,20 @@ import { sortingAlgorithm } from "../types";
 
 export default class InsertionSorter extends Sorter {
   public sort(array: number[]): number[] {
-    return array;
+    let sortedArray = [...array];
+    let n = sortedArray.length;
+
+    for (let i = 1; i < n; i++) {
+      let current = sortedArray[i];
+      let j = i - 1;
+      while (j > -1 && current < sortedArray[j]) {
+        sortedArray[j + 1] = sortedArray[j];
+        j--;
+      }
+      sortedArray[j + 1] = current;
+    }
+
+    return sortedArray;
   }
   public getType(): sortingAlgorithm {
     return "insertion";
