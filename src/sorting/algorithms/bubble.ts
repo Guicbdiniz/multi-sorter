627d52d40fe3ef7c2d97cc7c3939916c28a65c5e
Guicbdiniz <guicbdeniz@hotmail.com>
2022-09-25 18:29:07 -0300

    feat: add bubble sort

--- src/sorting/algorithms/bubble.ts
+++ src/sorting/algorithms/bubble.ts
@@ -3,7 +3,30 @@ import { sortingAlgorithm } from "../types";
 
 export default class BubbleSorter extends Sorter {
   public sort(array: number[]): number[] {
-    return array;
+    let i, j;
+    let sortedArray = [...array];
+    const arrayLength = sortedArray.length;
+
+    let isSwapped = false;
+
+    for (i = 0; i < arrayLength; i++) {
+      isSwapped = false;
+
+      for (j = 0; j < arrayLength; j++) {
+        if (sortedArray[j] > sortedArray[j + 1]) {
+          let temp = sortedArray[j];
+          sortedArray[j] = sortedArray[j + 1];
+          sortedArray[j + 1] = temp;
+          isSwapped = true;
+        }
+      }
+
+      if (!isSwapped) {
+        break;
+      }
+    }
+
+    return sortedArray;
   }
   public getType(): sortingAlgorithm {
     return "bubble";
